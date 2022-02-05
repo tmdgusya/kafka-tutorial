d05f9b41d92ccbabd39a55eaddba6b3e2745069d
roach <tmdgusya@naver.com>
2022-02-05 16:45:27 +0900

    add kafka-producer-with-key shell

---
+++ command/kafka-producer-with-key.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+topicName=$1
+
+sh ../kafka_2.12-2.3.0/bin/kafka-console-producer.sh \
+--broker-list localhost:9092 \
+--topic $topicName \
+--property "parse.key=true" \
+--property "key.separator=:"
